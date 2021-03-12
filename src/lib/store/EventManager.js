c3fea5572d3370278fbd7836890cef28e152ef8d
Maczuga <maczugapl@gmail.com>
2021-03-12 12:20:30 +0100

    Custom reusable event manager.

---
+++ src/lib/store/EventManager.js
@@ -0,0 +1,23 @@
+export class EventManager {
+  constructor() {
+    this.events = {};
+  }
+
+  eventExists(event) {
+    return Object.prototype.hasOwnProperty.call(this.events, event);
+  }
+
+  publish(event, data = {}) {
+    if (!this.eventExists(event))
+      return [];
+
+    this.events[event].forEach(callback => callback(data));
+  }
+
+  subscribe(event, callback) {
+    if (!this.eventExists(event))
+      this.events[event] = [];
+
+    this.events[event].push(callback);
+  }
+}
