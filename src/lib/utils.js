a659d6f64e0b80fb0c7cd28d753fc96994a021a7
Maczuga <maczugapl@gmail.com>
2021-03-13 17:15:21 +0100

    Missed one.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,13 @@
 export const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const strToBool = (str) => {
+  if (str === true)
+    return true;
+
+  if (str === false)
+    return false;
+
+  return str === "true";
+};
